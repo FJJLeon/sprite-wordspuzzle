b94292437be2c4bbd5b064aa4f5cbc55436c7aff
Junjie Fang <695048014@qq.com>
2021-01-20 01:09:41 +0800

    test time and space consume

--- myapp/words_puzzle.py
+++ myapp/words_puzzle.py
@@ -1,8 +1,11 @@
 # -*- coding: UTF-8 -*-
 import json
 from collections import Counter
+import time
+import os, sys, psutil
 
 sprite_data_url = "SpriteData.json"
+answer_url = "answer.txt"
 
 
 def loads_sprite():
@@ -34,7 +37,7 @@ def find_candidate_names(puzzle, inverted_index):
     candidate_names = set()
     puzzle = puzzle.replace("\t", "")
     for word in puzzle:
-        candidate_names |= inverted_index[word]
+        candidate_names |= inverted_index.get(word, set())
 
     return list(candidate_names)
 
@@ -85,7 +88,53 @@ def main_v2():
             break
 
 
+def loads_test_puzzles():
+    puzzles = []
+    with open(answer_url, mode='r') as load_file:
+        lines = load_file.readlines()
+        for i in range(0, len(lines), 6):
+            # print(lines[i])
+            puzzles.append(lines[i].replace("\t", "").replace("\n", "").replace(" ", ""))
+    return puzzles
+
+
+def testcase_v1():
+    test_puzzles = loads_test_puzzles()
+
+    # 1
+    v1_start = time.time()
+    v1_start_mem = psutil.Process(os.getpid()).memory_info().rss
+    sprite_names, _ = loads_sprite()
+    for puzzle in test_puzzles:
+        solve_puzzle(puzzle, sprite_names)
+    v1_end = time.time()
+    v1_end_mem = psutil.Process(os.getpid()).memory_info().rss
+    print(sys.getsizeof(sprite_names))
+    print("v1 cost: %f" % (v1_end - v1_start))
+    print((v1_end_mem - v1_start_mem) / 1024)
+
+
+def testcase_v2():
+    test_puzzles = loads_test_puzzles()
+
+    # 2
+    v2_start = time.time()
+    v2_start_mem = psutil.Process(os.getpid()).memory_info().rss
+    sprite_names, sprite_names_counter = loads_sprite()
+    inverted_index = create_inverted_index(sprite_names)
+    for puzzle in test_puzzles:
+        candidate_name = find_candidate_names(puzzle, inverted_index)
+        solve_puzzle(puzzle, candidate_name)
+    v2_end = time.time()
+    v2_end_mem = psutil.Process(os.getpid()).memory_info().rss
+    print(sys.getsizeof(inverted_index) / 1024)
+    print("v2 cost: %f" % (v2_end - v2_start))
+    print((v2_end_mem - v2_start_mem) / 1024)
+
+
 if __name__ == '__main__':
     # main()
     # test_inverted_index()
-    main_v2()
+    # main_v2()
+    # print(loads_test_puzzles())
+    testcase_v2()
